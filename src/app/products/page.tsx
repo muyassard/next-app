d3bcc13f0be1599ccfed45d8823ff5b9c3d89e94
muyassard <muyassarsolijonova@gmail.com>
2024-04-21 18:39:13 +0500

    *

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -10,6 +10,7 @@ const ProductsPage = async () => {
   return (
     <div className="px-[135px]">
       <Feature />
+      <div className=""></div>
       <section className="flex flex-col space-y-12 ">
         <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
           {products.map(prod => (
